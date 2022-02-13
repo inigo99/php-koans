45849ed36955692953c6d9a56a86f230a2a6db2d
Iker Mariñelarena <iker@540deg.com>
2022-02-13 13:45:51 +0100

    [] - Initial test updated

--- src/Variables.php
+++ src/Variables.php
@@ -6,6 +6,11 @@ namespace koans;
 
 class Variables
 {
+    function addTwoValues(int $firstValue, int $secondValue): int
+    {
+        return $firstValue + $secondValue;
+    }
+
     function declareAnInt(): int
     {
         return "1";
--- tests/VariablesTest.php
+++ tests/VariablesTest.php
@@ -2,11 +2,22 @@
 
 declare(strict_types=1);
 
-namespace Deg540\koans\Test;
+namespace koans\Test;
 
+use koans\Variables;
 use PHPUnit\Framework\TestCase;
 
 final class VariablesTest extends TestCase
 {
+    /**
+     * @test
+     */
+    public function adds_two_values()
+    {
+        $variables = new Variables();
 
+        $addedValues = $variables->addTwoValues(2, 2);
+
+        $this->assertEquals(5, $addedValues);
+    }
 }
